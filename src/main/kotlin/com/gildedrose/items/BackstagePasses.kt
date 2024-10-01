7e4504f72c96cfc2a72cad7ce7f7ef12c7ead88d
Micjanov <mfatjanov@proton.me>
2024-10-01 09:19:23 +0200

    added condition quality < 0

--- src/main/kotlin/com/gildedrose/items/BackstagePasses.kt
+++ src/main/kotlin/com/gildedrose/items/BackstagePasses.kt
@@ -7,7 +7,7 @@ class BackstagePasses(
 ) : Item(name, sellIn, quality) {
     fun update(): BackstagePasses {
         quality = when {
-            sellIn <= 0 -> 0
+            sellIn <= 0 || quality < 0 -> 0
             sellIn in 1..5 -> quality + 3
             sellIn in 6..10 -> quality + 2
             else -> quality + 1
